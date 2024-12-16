2ea8c992a08fe61a9fa3dbbe60d7f047dad48548
Communist <kirixa87@gmail.com>
2024-12-16 13:10:24 +0300

    gay

---
+++ parser/parser.h
@@ -0,0 +1,18 @@
+
+#include <string>
+#include <vector>
+#include <complex>
+
+namespace parsing_tool
+{
+    
+    std::vector< std::vector< std::complex<double>>> parse (const std::string &path) noexcept;
+ 
+} // namespace parsing_tool
+
+
+namespace name
+{
+    
+} // namespace name
+
