f66f605c893a9afac4b89575a8afc3569c8b0686
Nikita Gorin <exprompar@gmail.com>
2024-12-29 17:31:26 +0300

    ыфвыфв

--- reaction_rates.cpp
+++ reaction_rates.cpp
@@ -7,21 +7,21 @@
 #include <bits/stdc++.h>
 #include <complex>
 
-const int SPECIES_COUNT = 7;
+int SPECIES_COUNT = 7;
 const int REACTION_COUNT = 7;
 double T = 3000.0;  // Temperature in K
 const double RHO = 1000.0;  // Density in kg/m^3
 const double R = 8.31446;  // Universal gas constant in J/(mol·K)
 
 // Molecular masses in g/mol
-const double M[SPECIES_COUNT] = {
-    1.00784,    // H
-    15.999,     // O
-    17.00734,   // OH
-    2.01568,    // H2
-    31.998,     // O2
-    18.01528,   // H2O
-    28.014      // N2
+const double M[7] = {
+    2.01568 * 10e-3,    // H2
+    31.998 * 10e-3,     // O2
+    18.01528 * 10e-3,   // H2O
+    17.00734 * 10e-3,   // OH
+    1.00784 * 10e-3,    // H
+    15.999 * 10e-3,     // O
+    28.014 * 10e-3      // N2
 };
 
 class ReactionSystem {
@@ -78,7 +78,24 @@ public:
         }
         return gamma;
     }
+    std::pair<vector<double>,vector<double>> calcKmatr()
+    {
+        vector<double> Kb = {};
+        vector<double> Kf = {};
+        for (int r = 0; r < REACTION_COUNT; ++r) {
+            // Calculate forward and backward rate coefficients
+            double kf = calculateK(kinetic_params[r][0],
+                                kinetic_params[r][1],
+                                kinetic_params[r][2]);
+            double kb = calculateK(kinetic_params[r][3],
+                                kinetic_params[r][4],
+                                kinetic_params[r][5]);
+            Kf.push_back(kf);
+            Kb.push_back(kb);
 
+        }
+        return std::make_pair(Kf, Kb);
+    }
     // Calculate the Jacobian matrix
     std::vector<std::vector<double>> calculateJacobian(const std::vector<double>& gamma) {
         std::vector<std::vector<double>> jacobian(SPECIES_COUNT, std::vector<double>(SPECIES_COUNT, 0.0));
@@ -128,6 +145,7 @@ public:
     }
 };
 
+
 int test() {
     std::vector<std::vector<double>> A = {
         {6, 8, 2, 6, 34, 13},
@@ -157,94 +175,175 @@ int main() {
     std::vector<double> rho(SPECIES_COUNT, RHO/SPECIES_COUNT);  // Distribute density equally among species
 
     // Calculate gamma values
-    
+
 
     //if (!has_valid_values) {
     //  std::cout << "\nWarning: Jacobian matrix contains no valid finite values!" << std::endl;
     //     return 1;
         //}
 
+
+//ВОТ ЗДЕСЬ ТИПА ГАММЫ ОК ДА ОК ДА ДА ИЛИ НЕТ СПРАШИВАЮ ОК ДА ИЛИ СУКА НЕТ ДА ИЛИ НЕТ
+    auto g = vector<double> {2./7. * M[0], 2./7. * M[1], 0, 0, 0, 4./7.* M[6]}; //system.calculateGamma(rho);
+    auto [Kf, Kr] = system.calcKmatr();
+
+
+
+    for(int i = 0; i < g.size(); i++)
+        cout << g[i] << '\t';
+
     std::vector<std::pair<double,double>> res = std::vector<std::pair<double, double>>();
     for(double i = 300; i <= 6000; i+=300)
     {
         T = i;
-        auto gamma = system.calculateGamma(rho);
+//ВОТ ЗДЕСЬ ОГРОМНЫЙ ХУЙ
+        std::vector<std::vector<double>> jacobian = std::vector<std::vector<double>>
+        {
+        {
+            -Kf[0] * g[1] - Kf[2] * g[3] - Kf[3] * g[4],
+            -Kf[0] * g[0],
+            Kr[2] * g[4],
+            Kr[0] * 2 * g[3] - Kf[2] * g[0] + Kr[3] * g[4],
+            Kr[2] * g[2] + Kr[3] * g[3] + Kf[6] * 2 * g[4] * g[6],
+            -Kf[3] * g[0],
+            -Kr[6] * g[0] + Kf[6] * g[4] * g[4]
+        },
+        {
+            -Kf[0] * g[1],
+            -Kf[0] * g[0] - Kf[1] * g[4],
+            0,
+            2 * Kr[0] * g[3] + Kr[1] * g[4],
+            -Kf[1] * g[1],
+            Kr[1] * g[3],
+            0
+        },
+        {
+            Kf[2] * g[3],
+            0,
+            -Kr[2] * g[4] - Kr[4] * g[5] - Kr[5] * g[6],
+            Kf[2] * g[0] + Kr[4] * 2 * g[3] + Kf[5] * g[4] * g[6],
+            -Kr[2] * g[2] + Kf[5] * g[3] * g[6],
+            -Kr[4] * g[2],
+            Kf[5] * g[6]
+        },
+        {
+            2 * Kf[0] * g[1] - Kr[3] * g[4] + Kr[5] * g[5],
+            2 * Kf[0] * g[0],
+            Kr[2] * g[4] + 2 * Kr[5] * g[5] + Kr[6] * g[6],
+            -4 * Kr[0] * g[3] - Kr[4] * g[5] - Kf[0] * g[0] - Kr[3] * g[4] - 4 * Kr[5] * g[5] - Kf[5] * g[4] * g[6],
+            Kf[1] * g[1] + Kr[2] * g[2] - Kr[4] * g[3] - Kf[5] * g[3] * g[6],
+            -Kr[4] * g[3] + Kf[0] * g[0] + 2 * Kr[2] * g[2],
+            Kr[6] * g[2]
+        },
+        {
+            Kr[3] * g[4] + Kr[5] * g[5] + 2 * Kr[6] * g[6],
+            -Kf[1] * g[4],
+            -Kr[3] * g[4] + Kr[6] * g[6],
+            Kr[5] * g[5] + Kr[0] * g[0] - Kr[3] * g[4] - Kf[3] * g[4] * g[6],
+            -Kf[1] * g[1] - Kf[2] * g[2] - Kr[4] * g[3] - Kr[5] * g[3] * g[6] - 4 * Kr[5] * g[5] * g[6],
+            Kr[4] * g[3] + Kr[0] * g[0],
+            0
+        },
+        {
+            -Kr[5] * g[5],
+            Kr[3] * g[4],
+            -Kr[5] * g[5],
+            -Kr[5] * g[5] + Kr[3] * g[4] + 2 * Kr[0] * g[3],
+            Kf[1] * g[1] + Kr[4] * g[3],
+            -Kr[4] * g[3] - Kf[0] * g[0] - Kr[2] * g[2],
+            0
+        },
+        {
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        }
+        };
+        std::cout << '\n';
+        vector<vector<double>> jacob2 = vector<vector<double>>(6,vector<double>(6,0));
+        for(int i = 0; i < 7; i++)
+        {
+            for(int j = 0; j < 7; j++)
+            {
+                std::cout << scientific << showpoint << setprecision(2) << jacobian[i][j] << "\t";
+            }
+            std::cout << '\n';
+        }
+        std::cout << "T: " << T << '\n';
+        for(int k = 1; k <= 6; k++)
+            std::cout << "gamma " << k << "= " << '\n';
+        std::cout << "coef: " << '\n';
+
+        //ТЫ МЕНЯ УВАЖАЕШЬ ИЛИ НЕТ УВАЖАЕШЬ ИЛИ НЕТ
+
         // for (int i = 0; i < SPECIES_COUNT; ++i) {
         //     std::cout << "Gamma[" << i << "]: " << gamma[i] << std::endl;
         // }
 
         // Calculate Jacobian
-        auto jacobian = system.calculateJacobian(gamma);
+        //auto jacobian = system.calculateJacobian(gamma);
         
         // Print Jacobian matrix
         //std::cout << "Jacobian matrix dW_i/dgamma_j:" << std::endl;
-        bool has_valid_values = false;
-        for (int i = 0; i < SPECIES_COUNT; ++i) {
-            for (int j = 0; j < SPECIES_COUNT; ++j) {
-            //  std::cout.precision(4);
-                //std::cout << std::setw(10) << jacobian[i][j] << "\t";
-                if (std::isfinite(jacobian[i][j])) {
-                    has_valid_values = true;
-                }
-                if(std::fabs(jacobian[i][j]) < 1e-5)
-                    jacobian[i][j] = 0;
-            }
-            //  std::cout << std::endl;
-        }
+       
         //Нужно вычеркнуть третий столбец и третью строку
-        std::cout << "Якобиан (c вычеркнутым столбцом и строкой): \n";
-        auto jacob2 = std::vector<std::vector<double>>(jacobian.size() - 1, std::vector<double>(jacobian.size() - 1, 0));
-        int l = 0;
-        int k = 0;
-        for (int i = 0; i < SPECIES_COUNT; ++i) {
-            l = 0;
-            for (int j = 0; j < SPECIES_COUNT; ++j) {
-                if(i == 2)
-                    i += 1;
-                if(j == 2)
-                    j += 1;
-                if (std::isfinite(jacobian[i][j])) {
-                    has_valid_values = true;
-                }
-                if(std::fabs(jacobian[i][j]) < 1e-5)
-                    jacobian[i][j] = 0;
-                jacob2[k][l] = jacobian[i][j];
-                std::cout << std::scientific << std::setprecision(2) << std::showpoint << jacobian[i][j] << "\t";
-                l++;
-            }
-            std::cout << '\n';
-            k++;
-            //  std::cout << std::endl;
-        }
+        // std::cout << "Якобиан (c вычеркнутым столбцом и строкой): \n";
+        // auto jacob2 = std::vector<std::vector<double>>(jacobian.size() - 1, std::vector<double>(jacobian.size() - 1, 0));
+        // int l = 0;
+        // int k = 0;
+        // for (int i = 0; i < SPECIES_COUNT; ++i) {
+        //     l = 0;
+        //     for (int j = 0; j < SPECIES_COUNT; ++j) {
+        //         if(i == 2)
+        //             i += 1;
+        //         if(j == 2)
+        //             j += 1;
+        //         if (std::isfinite(jacobian[i][j])) {
+        //             has_valid_values = true;
+        //         }
+        //         if(std::fabs(jacobian[i][j]) < 1e-5)
+        //             jacobian[i][j] = 0;
+        //         jacob2[k][l] = jacobian[i][j];
+        //         std::cout << std::scientific << std::setprecision(2) << std::showpoint << jacobian[i][j] << "\t";
+        //         l++;
+        //     }
+        //     std::cout << '\n';
+        //     k++;
+        //     //  std::cout << std::endl;
+        // }
         // Calculate eigenvalues using QR algorithm
         //std::cout << "\nEigenvalues of the Jacobian matrix:" << std::endl;
-        auto [eigenvalues, A] = get_eigens(jacob2);
+        // auto [eigenvalues, A] = get_eigens(jacobian);
 
-        for (int i = 0; i < SPECIES_COUNT - 1; ++i) {
-            std::cout << "lambda_" << i + 1 << std::scientific << std::setprecision(2) << std::showpoint << " = " << std::fixed << std::setprecision(6)
-                  << eigenvalues[i].real() << " + " << eigenvalues[i].imag() << "i" << std::endl;
-        }
+        // for (int i = 0; i < SPECIES_COUNT; ++i) {
+        //     std::cout << "lambda_" << i + 1 << std::scientific << std::setprecision(2) << std::showpoint << " = " << std::fixed << std::setprecision(6)
+        //           << eigenvalues[i].real() << " + " << eigenvalues[i].imag() << "i" << std::endl;
+        // }
 
-        // find min and max abs eigenvalues
-        double min_abs_eigenvalue = std::fabs(eigenvalues[0].real());
-        double max_abs_eigenvalue = std::fabs(eigenvalues[0].real());
-        for (int i = 1; i < SPECIES_COUNT -1; ++i) {
-            if(std::abs(eigenvalues[i].real()) != 0){
-                min_abs_eigenvalue = std::min(min_abs_eigenvalue, std::abs(eigenvalues[i].real()));
-            }
+        // // find min and max abs eigenvalues
+        // double min_abs_eigenvalue = std::fabs(eigenvalues[0].real());
+        // double max_abs_eigenvalue = std::fabs(eigenvalues[0].real());
+        // for (int i = 1; i < SPECIES_COUNT; ++i) {
+        //     if(std::abs(eigenvalues[i].real()) != 0){
+        //         min_abs_eigenvalue = std::min(min_abs_eigenvalue, std::abs(eigenvalues[i].real()));
+        //     }
 
-            max_abs_eigenvalue = std::max(max_abs_eigenvalue, std::abs(eigenvalues[i].real()));
-        }
-        res.push_back({i, max_abs_eigenvalue / min_abs_eigenvalue});
+        //     max_abs_eigenvalue = std::max(max_abs_eigenvalue, std::abs(eigenvalues[i].real()));
+        // }
+        // res.push_back({i, max_abs_eigenvalue / min_abs_eigenvalue});
 
-        std::cout << "T: "<< i << " " << "Stiffness: " << max_abs_eigenvalue / min_abs_eigenvalue << std::endl;
-        std::cout << "----------------------------------------------------------------------------------" << std::endl;
+        // std::cout << "T: "<< i << " " << "Stiffness: " << max_abs_eigenvalue / min_abs_eigenvalue << std::endl;
+        // std::cout << "----------------------------------------------------------------------------------" << std::endl;
     }
-    std::cout << res.size() << std::endl;
-    for(int i = 0; i < res.size(); i++)
-        std::cout << res[i].first << " ";
-    std::cout << std::endl;
-    for(int i = 0; i < res.size(); i++)
-        std::cout << res[i].second << " ";
-    return 0;
+    // std::cout << res.size() << std::endl;
+    // for(int i = 0; i < res.size(); i++)
+    //     std::cout << res[i].first << " ";
+    // std::cout << std::endl;
+    // for(int i = 0; i < res.size(); i++)
+    //     std::cout << res[i].second << " ";
+     return 0;
 }
